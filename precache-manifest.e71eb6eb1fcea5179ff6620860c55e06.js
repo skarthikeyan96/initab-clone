887e5f9729c1f1aac2b989c001141cea33bbe440
karthikeyan <karthikeyan@bangthetable.com>
2019-09-14 17:21:38 +0530

    Updates

---
+++ precache-manifest.e71eb6eb1fcea5179ff6620860c55e06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27588acbaa18ea9f9476",
+    "url": "/initab-clone/static/js/runtime~main.543fa179.js"
+  },
+  {
+    "revision": "103f00dda6cf5ef0074a",
+    "url": "/initab-clone/static/js/main.0810b3d2.chunk.js"
+  },
+  {
+    "revision": "180bab4142a0d6cf850f",
+    "url": "/initab-clone/static/js/2.8230226a.chunk.js"
+  },
+  {
+    "revision": "103f00dda6cf5ef0074a",
+    "url": "/initab-clone/static/css/main.7c86e872.chunk.css"
+  },
+  {
+    "revision": "3b7c52cf651a5ee846c2a8d48acc099e",
+    "url": "/initab-clone/index.html"
+  }
+];
